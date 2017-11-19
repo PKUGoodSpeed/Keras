ae0b00449407d5606de493593426b6d8aaacabef
PKUGoodSpeed <zebo@ginkgo-llc.com>
2017-11-19 00:20:42 -0800

    sdf

--- aggr_stgy/tx_aggr/tx_cnn.py
+++ aggr_stgy/tx_aggr/tx_cnn.py
@@ -223,10 +223,10 @@ def main():
     axes[1][1].set_xlabel('# of steps')
     axes[1][1].legend()
     
-    plt.savefig('btrcnn_output/convrg_rst.png')
+    plt.savefig('tx_cnn_output/convrg_rst.png')
     
     ## show model configuration
-    plot_model(model, to_file = 'btrcnn_output/model.png')
+    plot_model(model, to_file = 'tx_cnn_cnn_output/model.png')
     
     
 
